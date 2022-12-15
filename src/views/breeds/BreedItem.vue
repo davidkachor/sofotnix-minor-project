60e18b9be03cec0e2cd2bbb267a6dcd494b8a693
davidkachor <kachor.david@gmail.com>
2022-12-15 11:24:33 +0200

    prove code with components

--- src/views/breeds/BreedItem.vue
+++ src/views/breeds/BreedItem.vue
@@ -5,50 +5,15 @@
         :tab-list="[{name: 'breeds', url: {name: $routeNames.breeds}}, {name: id}]"
       />
 
-      <div class=" relative">
-        <el-image :src="breed?.image.url" alt="item" class="min-h-[200px] w-full rounded-[20px]">
-          <template #error>
-            <div class="w-full h-full flex items-center justify-center bg-gray-200 p-[60px]">
-              <ImagePlaceholderIcon class="w-10 h-10" />
-            </div>
-          </template>
-        </el-image>
+      <BreedImageAndTitle :url="breed?.image.url" :name="breed?.name" />
 
-        <h2
-          class="absolute left-[50%] translate-x-[-50%] bottom-[-20px] shadow-xl bg-white
-          font-medium text-contrast text-4xl leading-[52px] px-10 py-[5px] text-center rounded-[20px]"
-        >
-          {{ breed?.name }}
-        </h2>
-      </div>
-
-      <section
-        class="mt-7 border-solid border-[2px] border-common p-10
-        rounded-[20px] flex flex-col gap-medium"
-      >
-        <p class="text-center font-medium text-pale text-xl">{{ breed?.bred_for }}</p>
-
-        <div class="grid grid-cols-2 gap-medium">
-          <div>
-            <p class="text-pale">
-              <strong class="text-contrast font-medium">Temperament:</strong><br>
-              {{ breed?.temperament }}
-            </p>
-          </div>
-
-          <div>
-            <p class="text-pale">
-              <strong class="text-contrast font-medium">Height:</strong> {{ breed?.height.metric }}
-            </p>
-            <p class="text-pale">
-              <strong class="text-contrast font-medium">Weight:</strong> {{ breed?.weight.metric }}
-            </p>
-            <p class="text-pale">
-              <strong class="text-contrast font-medium">Life span:</strong> {{ breed?.life_span }}
-            </p>
-          </div>
-        </div>
-      </section>
+      <BreedInfo
+        :bred-for="breed?.bred_for"
+        :height="breed?.height.metric"
+        :weight="breed?.weight.metric"
+        :temperament="breed?.temperament"
+        :life-span="breed?.life_span"
+      />
     </div>
   </MainContentLayout>
 </template>
---
+++ src/views/breeds/components/BreedInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <section
+    class="mt-7 border-solid border-[2px] border-common p-10
+        rounded-[20px] flex flex-col gap-medium"
+  >
+    <p class="text-center font-medium text-pale text-xl">{{ bredFor }}</p>
+
+    <div class="grid grid-cols-2 gap-medium">
+      <div>
+        <p class="text-pale">
+          <strong class="text-contrast font-medium">Temperament:</strong><br>
+          {{ temperament }}
+        </p>
+      </div>
+
+      <div>
+        <p class="text-pale">
+          <strong class="text-contrast font-medium">Height:</strong> {{ height }}
+        </p>
+        <p class="text-pale">
+          <strong class="text-contrast font-medium">Weight:</strong> {{ weight }}
+        </p>
+        <p class="text-pale">
+          <strong class="text-contrast font-medium">Life span:</strong> {{ lifeSpan }}
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  bredFor?: string
+  temperament?: string
+  height?: string
+  weight?: string
+  lifeSpan?: string
+}>()
+</script>
